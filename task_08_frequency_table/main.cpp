31c4b987f1df8b662c49bb80a8a51a823b20f79a
jakub-spec <jakubko.trnovec@gmail.com>
2026-01-20 09:06:09 +0100

    s

--- task_08_frequency_table/main.cpp
+++ task_08_frequency_table/main.cpp
@@ -5,35 +5,26 @@
 
 using namespace std;
 int main() {
-    int number[] = {1, 2, 3, 4, 5, 1, 2, 3, 4, 5, 1, 2, 3, 4, 5, 1, 2, 3, 4, 5};
 
-    cout << sizeof(number) / 4 << '\n';
+    int n, pocet[100] = {0};
+    cout << " nacitaj pocet cisel" << endl;
+    cin >> n;
 
-    for (int i = 0; i < 20; i++) {
-        cout << number[i] << '\n';
+    cout << " nacitaj cisla v rozsahu od 0 do 100 " << endl;
+    for (int i = 1; i < n; i++) {
+        int hodnota;
+        cin >> hodnota;
+        if (hodnota >= 0 && hodnota <= 100) {
+            pocet[hodnota]++;
+        }
+    }
+    cout << " pocet cisel s rovnakou hodnotou" << endl;
+    for (int hodnota = 0; hodnota <= 100; hodnota++) {
+        if (pocet[hodnota] > 0)
+            cout << hodnota << " " << pocet[hodnota] << endl;
     }
 
-    using namespace std;
-    int main() {
-        int n, pocet[100] = {0};
-        cout << " nacitaj pocet cisel" << endl;
-        cin >> n;
+    // TODO: read n values in [0,100], count, and print only those with count>0 in ascending value
 
-        cout << " nacitaj cisla v rozsahu od 0 do 100 " << endl;
-        for (int i = 0; i < n; i++) {
-            int hodnota;
-            cin >> hodnota;
-            if (hodnota >= 0 && hodnota <= 100) {
-                pocet[hodnota]++;
-            }
-        }
-        cout << " pocet cisel s rovnakou hodnotou" << endl;
-        for (int hodnota = 0; hodnota <= 100; hodnota++) {
-            if (pocet[hodnota] > 0)
-                cout << hodnota << " " << pocet[hodnota] << endl;
-        }
-
-        // TODO: read n values in [0,100], count, and print only those with count>0 in ascending value
-
-        return 0;
-    }
+    return 0;
+}
